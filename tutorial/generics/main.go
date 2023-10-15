9d1e1d34ef3899e59d0058c5403ceb7b7c1695e2
cpmachado <cpmachado@protonmail.com>
2023-10-15 10:29:56 +0100

    Remove type arguments when calling the generic function

--- tutorial/generics/main.go
+++ tutorial/generics/main.go
@@ -31,7 +31,6 @@ func main() {
 		"first":  34,
 		"second": 12,
 	}
-
 	floats := map[string]float64{
 		"first":  35.98,
 		"second": 26.99,
@@ -40,7 +39,12 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
 		SumInts(ints),
 		SumFloats(floats))
+
 	fmt.Printf("Generic Sums: %v and %v\n",
 		SumIntsOrFloats[string, int64](ints),
 		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
 }

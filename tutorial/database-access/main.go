bdaf75c239d1ffafc5f3f9d245167d734cf1653f
cpmachado <cpmachado@protonmail.com>
2023-10-29 15:43:19 +0000

    Query for a single row

--- tutorial/database-access/main.go
+++ tutorial/database-access/main.go
@@ -24,6 +24,7 @@ func main() {
 	if err := godotenvvault.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	// Capture connection properties.
 	cfg := mysql.Config{
 		User:   os.Getenv("DBUSER"),
@@ -49,6 +50,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -74,4 +81,18 @@ func albumsByArtist(name string) ([]Album, error) {
 	}
 
 	return albums, nil
+}
+
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
 }
